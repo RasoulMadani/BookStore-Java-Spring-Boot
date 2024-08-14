aecf531d6076386583de9d55781eb0ed78e3a9c8
Rasoulmadani <ali.rahbary@chmail.ir>
2024-08-14 10:33:06 +0330

    کانقیگ کتابخانه datasource-proxy برای دیدن بهتر کویری های اس کیو ال

--- src/main/java/ir/bookstore/config/DefaultConfig.java
+++ src/main/java/ir/bookstore/config/DefaultConfig.java
@@ -1,11 +1,19 @@
 package ir.bookstore.config;
 
+import net.ttddyy.dsproxy.listener.logging.SLF4JLogLevel;
+import net.ttddyy.dsproxy.listener.logging.SLF4JQueryLoggingListener;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+
+
+import javax.sql.DataSource;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class DefaultConfig {
@@ -27,4 +35,14 @@ public class DefaultConfig {
      public MessageSourceAccessor messageSourceAccessor(){
         return new MessageSourceAccessor(messageSource());
      }
+    @Bean
+    public DataSource dataSource(DataSourceProperties dataSourceProperties) {
+        DataSource originalDataSource = dataSourceProperties.initializeDataSourceBuilder().build();
+
+        return ProxyDataSourceBuilder
+                .create(originalDataSource)
+                .name("MyDS")
+                .listener(new SLF4JQueryLoggingListener())  // This logs queries in multiple lines
+                .build();
+    }
 }
